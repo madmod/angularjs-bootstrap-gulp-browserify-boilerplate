96046a26433286289f9221dc785257c387fb9ac0
Christopher Urquidi <christopher.urquidi@webdevlosangeles.com>
2016-01-11 16:11:26 -0800

    update home page with a basic bootstrap style

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -3,7 +3,8 @@ function ExampleCtrl() {
   // ViewModel
   const vm = this;
 
-  vm.title = 'AngularJS, Gulp, and Browserify! Written with keyboards and love!';
+  vm.title = 'AngularJS, Bootstrap 3, Gulp, and Browserify!';
+  vm.subTitle = 'Build something cool!';
   vm.number = 1234;
 
 }
