43c55e695077a40d081584294bc1068221710ecc
Christopher Urquidi <ChristopherUrquidi@users.noreply.github.com>
2016-01-11 20:59:11 -0800

    Update example.js

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -3,7 +3,7 @@ function ExampleCtrl() {
   // ViewModel
   const vm = this;
 
-  vm.title = 'AngularJS, Bootstrap 3, Gulp, and Browserify!';
+  vm.title = 'AngularJS, Bootstrap 3, Gulp, and Browserify.';
   vm.subTitle = 'Build something cool!';
   vm.number = 1234;
 
